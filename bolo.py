54b508562738341fe396866be236a5356f81ba0f
Siddhant Sinha <35433185+sid573@users.noreply.github.com>
2020-02-18 16:28:26 +0530

    added

--- bolo.py
+++ bolo.py
@@ -19,5 +19,5 @@ def uploadfile():
     elif request.method == 'GET':
         return "hello"
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
